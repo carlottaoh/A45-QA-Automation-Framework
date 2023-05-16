c525679cc86eb954794d174c08afb24d0dd03cd9
carlottaoh <carlotta.oh@testpro.io>
2023-05-15 19:25:29 -0700

    fixed hw25

--- src/test/java/Homework23.java
+++ src/test/java/Homework23.java
@@ -8,18 +8,18 @@ import pageRefactory.LoginPage;
 
 
 public class Homework23 extends BaseTest {
-    @Test
+    @Test (enabled = true, priority = 0, description = "Rename a playlist")
     public void renamePlaylist() {
         String newPlaylistName = "Your playlist";
         String email = "demo@class.com";
         String password = "te$t$tudent";
-        LoginPage loginPage = new LoginPage(driver);
-        HomePage homePage = new HomePage(driver);
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
 
         loginPage.enterCredentials(email, password).clickSubmit();
         Assert.assertTrue(homePage.avatarDisplayed());
         homePage.selectPlaylist().editNewPlaylistName(newPlaylistName);
-        WebElement findPlaylist = driver.findElement(By.xpath(".//a[text()='" + newPlaylistName + "']"));
+        WebElement findPlaylist = getDriver().findElement(By.xpath(".//a[text()='" + newPlaylistName + "']"));
         WebElement foundPlaylist = wait.until(ExpectedConditions.visibilityOf(findPlaylist));
         Assert.assertTrue(foundPlaylist.isDisplayed());
 
